53418779bd8ea84fe0b3e56532c5c5f5fead7f6d
CavGame <cavalctoretgiafi9@gmail.com>
2024-12-21 15:56:21 +0100

    StumbleGuys.py

---
+++ StumbleGuys.py
@@ -0,0 +1,8 @@
+# StumbleGuys.py
+def start_game():
+    print("Avvio Stumble Guys...")
+    # Logica per avviare Stumble Guys
+
+def play_round():
+    print("Gioco un round di Stumble Guys...")
+    # Logica per far giocare l'IA nel gioco
