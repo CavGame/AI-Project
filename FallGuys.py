3bdee905b4f3a6706d04995fb2f6a9513567480a
CavGame <cavalctoretgiafi9@gmail.com>
2024-12-21 15:55:57 +0100

    FallGuys.py

---
+++ FallGuys.py
@@ -0,0 +1,8 @@
+# FallGuys.py
+def start_game():
+    print("Avvio Fall Guys...")
+    # Logica per avviare Fall Guys
+
+def play_round():
+    print("Gioco un round di Fall Guys...")
+    # Logica per far giocare l'IA in un round del gioco
